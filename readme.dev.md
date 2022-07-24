c5f10127d5790e0baf6af11d87e76202188bccd6
Ishtmeet Singh <ish.rissam@gmail.com>
2022-07-24 17:48:18 +0530

    make use of debug assertions for lesser benchmark time during dev | refactor code
    fixes #5, fixes #4

---
+++ readme.dev.md
@@ -0,0 +1,102 @@
+# Rust framework benchmarks
+
+Benchmarks of most widely used [rust](https://rust-lang.org) web frameworks.
+
+# Demo
+![Demo](https://s4.gifyu.com/images/outputf55c6e3d5b6a1f8e.gif)
+
+
+## Frameworks included
+**[Hyper](https://hyper.rs)** - Fast and safe HTTP for the Rust language<br>
+**[Tokio-minihttp](https://github.com/tokio-rs/tokio-minihttp)** - Proof-of-concept implementation of a simple HTTP/1.1 server using Tokio.<br>
+# Results
+|   Concurrency: 128   |   Duration: 2 secs   |   Threads: 2   |
+|:-------------------:|:---------------------:|:--------------:|
+
+|   **Name**   |   Req/sec   | Avg Latency | Max Latency |  # Requests |
+|:------------:|:-----------:|:-----------:|:-----------:|:-----------:|
+|**Tokio-minihttp** |239,149|0.53ms|4.05ms|477,686|
+|**Hyper** |218,540|0.58ms|6.97ms|436,375|
+
+
+|   Concurrency: 256   |   Duration: 2 secs   |   Threads: 2   |
+|:-------------------:|:---------------------:|:--------------:|
+
+|   **Name**   |   Req/sec   | Avg Latency | Max Latency |  # Requests |
+|:------------:|:-----------:|:-----------:|:-----------:|:-----------:|
+|**Tokio-minihttp** |231,721|1.10ms|23.90ms|462,355|
+|**Hyper** |187,067|1.36ms|22.30ms|373,328|
+
+
+
+
+
+
+## Benchmarking tool
+The benchmarks have been performed using [rewrk](https://github.com/ChillFish8/rewrk), locally. 
+
+Check the raw output from rewrk [here](https://github.com/Ishtmeet-Singh/rust-framework-benchmarks/tree/master/perf).
+
+
+## Try it yourself
+Everything is automated, including adding a framework, generating `md` file output, and running the tests without having to start all the servers at once!
+
+Pleas make sure you do not have capped soft limit or hard limit for file descriptors, this may cause benchmarks with high concurrency (-c) fail with OS error 54,
+to fix that - 
+
+## Linux
+1. Open `/etc/sysctl.conf`
+2. Add `fs.file-max = 65536`
+3. Reboot your pc and verify it after rebooting with `sysctl -p`
+
+## Mac
+1. Check your current limit - `launchctl limit maxfiles`
+2. The first entry is the soft limit, and the second entry is hard limit. In most cases the hard limit is unlimited, it's the soft limit that we need to change
+3. Update the hard limit `sudo launchctl limit maxfiles 65536 200000`
+4. Reboot.
+
+Alternatively, if you wish to change the soft limit to only run the benchmark one time, you can change it for your current terminal session by
+`ulimit -n 65536`.
+This doesn't require boot, and will reset back to the default (usually 2560) after restarting the terminal.
+
+To run the tests locally, please follow the steps - 
+
+1. Download the repository as a zip, or clone/fork it.
+2. `cd rust-framework-benchmarks`
+3. `cargo build --release --bins`
+4. Run the main script and you're good to go..
+`./target/release/main` or `cargo run --release --bin main` 
+
+All the output will be stored in `perf/{name}/{concurrency}.txt*`
+
+## Machine used
+M1 Max MacBook Pro 2021 - 64GB ram, 10 CPU cores and 32 GPU cores
+
+## Suggestions and changes
+All the suggestions, code changes or additions of another web framework is appreciated. I'd like to keep the code as close as a real world scenario, instead of optimising it to the metal.
+
+To add a new library/framework, please make sure to use the `PORT` provided through the benchmark dynamically. Default is `3000` for all. You can change it in `config.json`.
+
+Also, to add a framework, add an entry inside `config.json` for the benchmarks to detect it.
+
+```json
+[
+  {
+    // Name of your framework. Displayed in the readme and during logs
+    "name": "Axum", 
+    // Default for all the frameworks
+    "port": 3000,
+    // the name of the file that your framework is located
+    "binary": "axum", 
+    // Github or Crates.io or Website
+    "url": "https://github.com/tokio-rs/axum" 
+  }
+]
+```
+
+```rs
+fn get_port_number() -> String {
+    std::env::args().collect::<Vec<String>>()[1].clone()
+}
+ Server::build().bind("hello-world", format!("127.0.0.1:{}", port_number))
+```
--- src/main.rs
+++ src/main.rs
@@ -21,30 +21,48 @@ macro_rules! infer_len_slice {
     }
 }
 
+#[cfg(not(debug_assertions))]
+static FRAMEWORK_SETTINGS: &str = include_str!("./utils/config.json");
+#[cfg(debug_assertions)]
+static FRAMEWORK_SETTINGS: &str = include_str!("./utils/config.dev.json");
+
+#[cfg(not(debug_assertions))]
+static OUTPUT_MD_FILE: &str = "./readme.md";
+#[cfg(debug_assertions)]
+static OUTPUT_MD_FILE: &str = "./readme.dev.md";
+
+#[cfg(not(debug_assertions))]
+const DEFAULT_DURATION: u32 = 40_u32;
+#[cfg(debug_assertions)]
+const DEFAULT_DURATION: u32 = 5_u32;
+
+static READ_ME_STRING: &str = include_str!("./utils/readme_block.txt");
+
 infer_len_slice !(static BENCHMARK_SETTINGS: [Settings; _] = [
+    #[cfg(not(debug_assertions))]
     Settings {
         concurrency: 16,
         threads: 2,
-        duration: 60,
+        duration: DEFAULT_DURATION,
     },
+    #[cfg(not(debug_assertions))]
     Settings {
         concurrency: 64,
         threads: 2,
-        duration: 60,
+        duration: DEFAULT_DURATION,
     },
     Settings {
         concurrency: 128,
         threads: 2,
-        duration: 60,
+        duration: DEFAULT_DURATION,
     },
     Settings {
         concurrency: 256,
         threads: 2,
-        duration: 60,
+        duration: DEFAULT_DURATION,
     },
 ]);
 
-static FRAMEWORK_SETTINGS: &str = include_str!("../config.json");
 struct Settings {
     concurrency: u32,
     threads: u8,
@@ -141,13 +159,14 @@ impl Framework {
             .for_each(|(bench_index, setting)| {
                 self.print_log(setting, framework_index, bench_index);
                 // wait 2 secs till the server starts running (some servers take more time to start - for example tide)
-                std::thread::sleep(Duration::from_secs(2));
+                std::thread::sleep(Duration::from_secs(1));
                 let rewrk_handle = Command::new("rewrk")
                     .arg(format!("-d{}s", setting.duration))
                     .arg(format!("--threads={}", setting.threads))
                     .arg(format!("-c{}", setting.concurrency))
                     .arg(format!("-h=http://localhost:{}", self.port))
                     .output();
+
                 let rewrk_output = rewrk_handle.unwrap();
                 if !rewrk_output.stderr.is_empty() {
                     println!("Error: {}", String::from_utf8_lossy(&rewrk_output.stderr));
@@ -162,7 +181,7 @@ impl Framework {
                     std::process::exit(-1);
                 }
                 // wait a bit to free system resources
-                std::thread::sleep(Duration::from_secs(2));
+                std::thread::sleep(Duration::from_secs(1));
             });
 
         if let Err(err_message) = server_handle.kill() {
@@ -187,10 +206,14 @@ async fn main() {
     write_readme(&frameworks);
 
     // optional - disable it if you've not forked yet
-    commit_and_push();
+    // disable auto commit while running in debug mode
+    if !cfg!(debug_assertions) {
+        commit_and_push();
+    }
 }
 
 fn write_readme(frameworks: &Vec<Framework>) {
+    println!("READ ME STRING: {}", READ_ME_STRING);
     let split_string: Vec<&str> = READ_ME_STRING.split("==SPLIT==").collect();
     let mut markdown_content = String::new();
 
@@ -226,7 +249,8 @@ fn write_readme(frameworks: &Vec<Framework>) {
         "{}\n{}\n{}",
         split_string[0], markdown_content, split_string[1]
     );
-    fs::write("./readme.md", new_md).unwrap();
+    println!("Writing to {}", OUTPUT_MD_FILE);
+    fs::write(OUTPUT_MD_FILE, new_md).unwrap();
 }
 
 fn commit_and_push() {
@@ -408,85 +432,3 @@ fn print_benchmark_message() {
 
 static MARKDOWN_HEADER: &str =
     "|   **Name**   |   Req/sec   | Avg Latency | Max Latency |  # Requests |\n|:------------:|:-----------:|:-----------:|:-----------:|:-----------:|";
-
-static READ_ME_STRING: &str = r##"
-# Rust framework benchmarks
-
-Benchmarks of most widely used [rust](https://rust-lang.org) web frameworks.
-
-# Demo
-![Demo](https://s4.gifyu.com/images/outputf55c6e3d5b6a1f8e.gif)
-
-==SPLIT==
-
-
-## Benchmarking tool
-The benchmarks have been performed using [rewrk](https://github.com/ChillFish8/rewrk), locally. 
-
-Check the raw output from rewrk [here](https://github.com/Ishtmeet-Singh/rust-framework-benchmarks/tree/master/perf).
-
-
-## Try it yourself
-Everything is automated, including adding a framework, generating `md` file output, and running the tests without having to start all the servers at once!
-
-Pleas make sure you do not have capped soft limit or hard limit for file descriptors, this may cause benchmarks with high concurrency (-c) fail with OS error 54,
-to fix that - 
-
-## Linux
-1. Open `/etc/sysctl.conf`
-2. Add `fs.file-max = 65536`
-3. Reboot your pc and verify it after rebooting with `sysctl -p`
-
-## Mac
-1. Check your current limit - `launchctl limit maxfiles`
-2. The first entry is the soft limit, and the second entry is hard limit. In most cases the hard limit is unlimited, it's the soft limit that we need to change
-3. Update the hard limit `sudo launchctl limit maxfiles 65536 200000`
-4. Reboot.
-
-Alternatively, if you wish to change the soft limit to only run the benchmark one time, you can change it for your current terminal session by
-`ulimit -n 65536`.
-This doesn't require boot, and will reset back to the default (usually 2560) after restarting the terminal.
-
-To run the tests locally, please follow the steps - 
-
-1. Download the repository as a zip, or clone/fork it.
-2. `cd rust-framework-benchmarks`
-3. `cargo build --release --bins`
-4. Run the main script and you're good to go..
-`./target/release/main` or `cargo run --release --bin main` 
-
-All the output will be stored in `perf/{name}/{concurrency}.txt*`
-
-## Machine used
-M1 Max MacBook Pro 2021 - 64GB ram, 10 CPU cores and 32 GPU cores
-
-## Suggestions and changes
-All the suggestions, code changes or additions of another web framework is appreciated. I'd like to keep the code as close as a real world scenario, instead of optimising it to the metal.
-
-To add a new library/framework, please make sure to use the `PORT` provided through the benchmark dynamically. Default is `3000` for all. You can change it in `config.json`.
-
-Also, to add a framework, add an entry inside `config.json` for the benchmarks to detect it.
-
-```json
-[
-  {
-    // Name of your framework. Displayed in the readme and during logs
-    "name": "Axum", 
-    // Default for all the frameworks
-    "port": 3000,
-    // the name of the file that your framework is located
-    "binary": "axum", 
-    // Github or Crates.io or Website
-    "url": "https://github.com/tokio-rs/axum" 
-  }
-]
-```
-
-```rs
-fn get_port_number() -> String {
-    std::env::args().collect::<Vec<String>>()[1].clone()
-}
- Server::build().bind("hello-world", format!("127.0.0.1:{}", port_number))
-```
-
-"##;
